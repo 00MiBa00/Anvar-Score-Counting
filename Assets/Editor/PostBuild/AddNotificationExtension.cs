23a48aa4f29c499d660081f165674c8f0133f1d3
Nikita <165208357+00MiBa00@users.noreply.github.com>
2025-06-05 13:15:51 +0200

    Added

--- Assets/Editor/PostBuild/AddNotificationExtension.cs
+++ Assets/Editor/PostBuild/AddNotificationExtension.cs
@@ -3,6 +3,7 @@ using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
 using UnityEditor.iOS.Xcode.Extensions;
 using System.IO;
+using System.Text;
 
 public class AddNotificationExtension
 {
@@ -22,7 +23,6 @@ public class AddNotificationExtension
       string mainTarget = project.TargetGuidByName("Unity-iPhone");
 #endif
 
-      // === Extension Setup ===
       string extensionTargetName = "notifications";
       string extensionBundleId = PlayerSettings.GetApplicationIdentifier(BuildTargetGroup.iOS) + ".notifications";
       string extensionFolder = Path.Combine(path, "NotificationService");
@@ -36,52 +36,49 @@ public class AddNotificationExtension
       string relativeSwiftPath = "NotificationService/NotificationService.swift";
       string relativeEntitlementsExtensionPath = "NotificationService/notifications.entitlements";
 
-      File.WriteAllText(plistPath, @"<?xml version=""1.0"" encoding=""UTF-8""?>
-<!DOCTYPE plist PUBLIC ""-//Apple//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
-<plist version=""1.0""> 
-<dict> 
-  <key>NSExtension</key> 
-  <dict> 
-      <key>NSExtensionPointIdentifier</key> 
-      <string>com.apple.usernotifications.service</string> 
-      <key>NSExtensionPrincipalClass</key> 
-      <string>$(PRODUCT_MODULE_NAME).NotificationService</string> 
-  </dict> 
-</dict> 
-</plist>");
+      var plistContent = new StringBuilder();
+      plistContent.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+      plistContent.AppendLine("<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">");
+      plistContent.AppendLine("<plist version=\"1.0\">");
+      plistContent.AppendLine("<dict>");
+      plistContent.AppendLine("  <key>NSExtension</key>");
+      plistContent.AppendLine("  <dict>");
+      plistContent.AppendLine("    <key>NSExtensionPointIdentifier</key>");
+      plistContent.AppendLine("    <string>com.apple.usernotifications.service</string>");
+      plistContent.AppendLine("    <key>NSExtensionPrincipalClass</key>");
+      plistContent.AppendLine("    <string>$(PRODUCT_MODULE_NAME).NotificationService</string>");
+      plistContent.AppendLine("  </dict>");
+      plistContent.AppendLine("</dict>");
+      plistContent.AppendLine("</plist>");
+      File.WriteAllText(plistPath, plistContent.ToString());
 
-      File.WriteAllText(swiftPath, @"
-import UserNotifications
-import FirebaseMessaging
+      var swiftContent = new StringBuilder();
+      swiftContent.AppendLine("import UserNotifications");
+      swiftContent.AppendLine("import FirebaseMessaging");
+      swiftContent.AppendLine();
+      swiftContent.AppendLine("class NotificationService: UNNotificationServiceExtension {");
+      swiftContent.AppendLine("  var contentHandler: ((UNNotificationContent) -> Void)?");
+      swiftContent.AppendLine("  var bestAttemptContent: UNMutableNotificationContent?");
+      swiftContent.AppendLine();
+      swiftContent.AppendLine("  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {");
+      swiftContent.AppendLine("    self.contentHandler = contentHandler");
+      swiftContent.AppendLine("    bestAttemptContent = request.content.mutableCopy() as? UNMutableNotificationContent");
+      swiftContent.AppendLine();
+      swiftContent.AppendLine("    guard let bestAttemptContent = bestAttemptContent else { return }");
+      swiftContent.AppendLine();
+      swiftContent.AppendLine("    FIRMessagingExtensionHelper().populateNotificationContent(");
+      swiftContent.AppendLine("      bestAttemptContent,");
+      swiftContent.AppendLine("      withContentHandler: contentHandler)");
+      swiftContent.AppendLine("  }");
+      swiftContent.AppendLine();
+      swiftContent.AppendLine("  override func serviceExtensionTimeWillExpire() {");
+      swiftContent.AppendLine("    if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {");
+      swiftContent.AppendLine("      contentHandler(bestAttemptContent)");
+      swiftContent.AppendLine("    }");
+      swiftContent.AppendLine("  }");
+      swiftContent.AppendLine("}");
+      File.WriteAllText(swiftPath, swiftContent.ToString());
 
-class NotificationService: UNNotificationServiceExtension {
-  var contentHandler: ((UNNotificationContent) -> Void)?
-  var bestAttemptContent: UNMutableNotificationContent?
-
-  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-      self.contentHandler = contentHandler
-      bestAttemptContent = request.content.mutableCopy() as? UNMutableNotificationContent
-
-      guard let bestAttemptContent = bestAttemptContent else { return }
-
-      FIRMessagingExtensionHelper().populateNotificationContent(
-          bestAttemptContent,
-          withContentHandler: contentHandler)
-  }
-
-  override func serviceExtensionTimeWillExpire() {
-      if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
-          contentHandler(bestAttemptContent)
-      }
-  }
-}
-");
-
-      if (!File.Exists(swiftPath)) {
-          UnityEngine.Debug.LogError("🔥 Swift-файл не создан! " + swiftPath);
-      }
-
-      // === Add Extension Target and files ===
       string swiftFileGUID = project.AddFile(relativeSwiftPath, relativeSwiftPath, PBXSourceTree.Source);
       string extensionTarget = project.AddAppExtension(mainTarget, extensionTargetName, extensionBundleId, relativePlistPath);
 
@@ -102,15 +99,16 @@ class NotificationService: UNNotificationServiceExtension {
       RemoveEmbedAppExtensionsPhase(path, project, mainTarget);
       project.WriteToFile(projectPath);
 
-      // === Add capabilities to extension ===
-      File.WriteAllText(entitlementsExtensionPath, @"<?xml version=""1.0"" encoding=""UTF-8""?>
-<!DOCTYPE plist PUBLIC ""-//Apple//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
-<plist version=""1.0""> 
-<dict> 
-  <key>aps-environment</key> 
-  <string>development</string> 
-</dict> 
-</plist>");
+      var entitlementsContent = new StringBuilder();
+      entitlementsContent.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+      entitlementsContent.AppendLine("<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">");
+      entitlementsContent.AppendLine("<plist version=\"1.0\">");
+      entitlementsContent.AppendLine("<dict>");
+      entitlementsContent.AppendLine("  <key>aps-environment</key>");
+      entitlementsContent.AppendLine("  <string>development</string>");
+      entitlementsContent.AppendLine("</dict>");
+      entitlementsContent.AppendLine("</plist>");
+      File.WriteAllText(entitlementsExtensionPath, entitlementsContent.ToString());
 
       var extensionCapabilityManager = new ProjectCapabilityManager(
           projectPath,
@@ -124,17 +122,9 @@ class NotificationService: UNNotificationServiceExtension {
       project.ReadFromFile(projectPath);
       project.AddBuildProperty(extensionTarget, "CODE_SIGN_ENTITLEMENTS", relativeEntitlementsExtensionPath);
 
-      // === Add capabilities to main target ===
       string mainEntitlementsPath = Path.Combine(path, "main.entitlements");
       string relativeMainEntitlementsPath = "main.entitlements";
-      File.WriteAllText(mainEntitlementsPath, @"<?xml version=""1.0"" encoding=""UTF-8""?>
-<!DOCTYPE plist PUBLIC ""-//Apple//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
-<plist version=""1.0""> 
-<dict> 
-  <key>aps-environment</key> 
-  <string>development</string> 
-</dict> 
-</plist>");
+      File.WriteAllText(mainEntitlementsPath, entitlementsContent.ToString());
 
       var mainCapabilityManager = new ProjectCapabilityManager(
           projectPath,
@@ -175,29 +165,29 @@ class NotificationService: UNNotificationServiceExtension {
   {
       string podfilePath = Path.Combine(pathToBuiltProject, "Podfile");
 
-      string podfileContent = @"
-source 'https://cdn.cocoapods.org/'
+      var podfileBuilder = new StringBuilder();
+      podfileBuilder.AppendLine("source 'https://cdn.cocoapods.org/'");
+      podfileBuilder.AppendLine();
+      podfileBuilder.AppendLine("platform :ios, '13.0'");
+      podfileBuilder.AppendLine();
+      podfileBuilder.AppendLine("use_frameworks!");
+      podfileBuilder.AppendLine();
+      podfileBuilder.AppendLine("target 'UnityFramework' do");
+      podfileBuilder.AppendLine("  pod 'AppsFlyerFramework', '6.16.2'");
+      podfileBuilder.AppendLine("  pod 'FirebaseAnalytics', '11.10.0'");
+      podfileBuilder.AppendLine("  pod 'Firebase/Messaging', '11.10.0'");
+      podfileBuilder.AppendLine("end");
+      podfileBuilder.AppendLine();
+      podfileBuilder.AppendLine("target 'Unity-iPhone' do");
+      podfileBuilder.AppendLine("end");
+      podfileBuilder.AppendLine();
+      podfileBuilder.AppendLine("target 'notifications' do");
+      podfileBuilder.AppendLine("  pod 'FirebaseAnalytics', '11.10.0'");
+      podfileBuilder.AppendLine("  pod 'Firebase/Messaging', '11.10.0'");
+      podfileBuilder.AppendLine("end");
 
-platform :ios, '13.0'
-
-use_frameworks! :linkage => :static
-
-target 'UnityFramework' do
-pod 'AppsFlyerFramework', '6.16.2'
-pod 'FirebaseAnalytics', '11.10.0'
-pod 'Firebase/Messaging', '11.10.0'
-end
-
-target 'Unity-iPhone' do
-end
-
-target 'notifications' do
-pod 'FirebaseAnalytics', '11.10.0'
-pod 'Firebase/Messaging', '11.10.0'
-end
-";
-
-      File.WriteAllText(podfilePath, podfileContent);
+      File.WriteAllText(podfilePath, podfileBuilder.ToString());
+      File.WriteAllText(Path.Combine(pathToBuiltProject, "podfile_ready"), "ok");
   }
 
   private static void RemoveEmbedAppExtensionsPhase(string pathToBuiltProject, PBXProject project, string targetGuid)
